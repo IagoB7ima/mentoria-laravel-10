2fc490de2daa1ee8322fa24c9e33b48abd2cc6be
IagoB7ima <iagonumero1@gmail.com>
2023-06-12 23:52:29 -0300

    dashboard

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Cliente;
+use App\Models\Produto;
+use App\Models\Venda;
+use Illuminate\Http\Request;
+
+class DashboardController extends Controller
+{
+    public function index()
+    {
+        $totalDeProdutoCadastrado = $this->buscaTotalProdutoCadastrado();
+        $tatalDeClienteCadastrado = $this->buscaTotalClienteCadastrado();
+        $tatalDeVendaCadastrado = $this->buscaTotalVendaCadastrado();
+
+
+        return view('pages.dashboard.dashboard', compact('totalDeProdutoCadastrado', 'tatalDeClienteCadastrado', 'tatalDeVendaCadastrado'));
+    }
+
+    public function buscaTotalProdutoCadastrado()
+    {
+        $findProduto = Produto::all()->count();
+
+        return $findProduto;
+    }
+
+    public function buscaTotalClienteCadastrado()
+    {
+        $find = Cliente::all()->count();
+
+        return $find;
+    }
+
+    public function buscaTotalVendaCadastrado()
+    {
+        $find = Venda::all()->count();
+
+        return $find;
+    }
+}
